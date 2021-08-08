10e6f813b9365e2f5cdc7828235f1fb43aea53cf
joser <joser@pineal.ai>
2021-08-08 20:07:04 +0800

    no message

---
+++ Sources/SwiftKitUI/Layout/Spacer.swift
@@ -0,0 +1,12 @@
+//
+//  Spacer.swift
+//  
+//
+//  Created by joser on 2021/8/8.
+//
+
+import Foundation
+
+public struct Spacer: View {
+    
+}
--- Sources/SwiftKitUI/Layout/Stack.swift
+++ Sources/SwiftKitUI/Layout/Stack.swift
@@ -5,8 +5,14 @@
 //  Created by joser on 2021/8/6.
 //
 
-import Foundation
+import UIKit
 
 public protocol Stack: View {
     
+}
+
+public extension UIView {
+    func addStack<S:Stack>(@ViewBuilder content: () -> S) {
+        let stack = content()
+    }
 }
